500a068279fbe5637f77ce808275dcbc5e6f8af4
Lydia Wong <wong.h.lydia@gmail.com>
2022-02-09 11:03:22 -0500

    fourth commit - added means for each variety

--- scripts_compiling/first_script.R
+++ scripts_compiling/first_script.R
@@ -4,4 +4,7 @@ y = 2
 animals <- read.csv("favAnimals.csv")
  
 dat <- read.csv("./data_raw/iris.csv")
- 
+head(dat)
+mean(dat$sepal.length)
+unique(dat$variety)
+tapply(dat$petal.length, dat$variety, mean)
