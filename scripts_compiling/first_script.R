d14d69d4bda2860b904019747b3803bbb1fc6cea
Lydia Wong <wong.h.lydia@gmail.com>
2022-02-22 18:30:44 -0500

    Second Commit

--- scripts_compiling/first_script.R
+++ scripts_compiling/first_script.R
@@ -1,11 +1,7 @@
-x = 1
-y = 2
-
-animals <- read.csv("favAnimals.csv")
+animals <- read.csv("./data_raw/favAnimals.csv")
  
 dat <- read.csv("./data_raw/iris.csv")
-head(dat)
-mean(dat$sepal.length)
+
 unique(dat$variety)
 tapply(dat$petal.length, dat$variety, mean)
 
@@ -13,5 +9,10 @@ require(reshape2)
 
 datMelt <- melt(dat, id = c("variety"))
 datCast <- dcast(datMelt, variety ~ variable, fun.aggregate = mean)
+
+plot(sepal.length ~ sepal.width, data = dat)
+
+dat$flower.number <- c(1:nrow(dat))
+head(dat)
 
                  
